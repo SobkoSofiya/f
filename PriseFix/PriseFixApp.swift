f334fddada06e2f9116728c2d2a54c6d4661993b
SobkoSofiya <sobkosofiya23@gmail.com>
2021-02-09 09:00:45 +0300

    Initial Commit

---
+++ PriseFix/PriseFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  PriseFixApp.swift
+//  PriseFix
+//
+//  Created by Sofi on 09.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct PriseFixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
